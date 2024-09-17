c9cecf28b5ba35cde309b3f56492c1a0bb03a691
Hugo Marthinet <h.marthinet@gmail.com>
2024-09-17 15:46:04 +0200

    Typos

--- R/MR.R
+++ R/MR.R
@@ -1,8 +1,8 @@
-MR <- function(n.obs, pop.ref,
+MR <- function(n.death, pop.ref,
                by = 100000,
                decimal = 1) {
 
-  res <- round(epi.conf(cbind(nb_ref , pop_tot), ctype = "inc.rate", method = "exact") * 100000, decimal)
+  res <- round(epi.conf(cbind(n.death , pop.ref), ctype = "inc.rate", method = "exact") * 100000, decimal)
 
   return(list(MR = res[, 1],
               CI = res[, -1]))
